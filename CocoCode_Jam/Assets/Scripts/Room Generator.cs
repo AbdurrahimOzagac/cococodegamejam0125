0b307a09a5c9e323aa0d850464720a51ef63fce0
Berkay ÖRENE <oreneberkay@gmail.com>
2025-02-02 21:41:10 +0300

    room generator updated

--- CocoCode_Jam/Assets/Scripts/Room Generator.cs
+++ CocoCode_Jam/Assets/Scripts/Room Generator.cs
@@ -36,15 +36,22 @@ public class RoomGenerator : MonoBehaviour
         roomWidth1 = Random.Range(10, 21);
         roomWidth2 = Random.Range(10, 21);
 
-        Vector3 position = new Vector3(playerPosition.x + roomWidth1/2 , playerPosition.y, playerPosition.z + 5);
-        Vector3 position2 = new Vector3(playerPosition.x - roomWidth1 /2 , playerPosition.y, playerPosition.z + 5);
+        Vector3 position1 = new Vector3(playerPosition.x + roomWidth1 / 2 + 1.5f, playerPosition.y, playerPosition.z + 5);
+        Vector3 position2 = new Vector3(playerPosition.x - roomWidth1 /2 - 1.5f, playerPosition.y, playerPosition.z + 5);
         Vector3 position3 = new Vector3(playerPosition.x + roomWidth1 , playerPosition.y, playerPosition.z + 5 + roomWidth2/2);
         Vector3 position4 = new Vector3(playerPosition.x - roomWidth1 , playerPosition.y, playerPosition.z + 5 + roomWidth2/2);
 
          
 
-        Instantiate(wall, position, Quaternion.identity);
-        Instantiate(wall, position2, Quaternion.identity);
+        Instantiate(wallWithDoor, new Vector3(playerPosition.x, playerPosition.y, playerPosition.z + 5), Quaternion.identity);
+        GameObject wallWithDoor1 = GameObject.Find("WallDoor(Clone)");
+        GameObject wallWithDoor1child1 = wallWithDoor1.gameObject.transform.Find("Wall 1").gameObject;
+        GameObject wallWithDoor1child2 = wallWithDoor1.gameObject.transform.Find("Wall 3").gameObject;
+        wallWithDoor1child1.transform.position = position1;
+        wallWithDoor1child2.transform.position = position2;
+
+        
+
         Instantiate(wall, position3, Quaternion.identity);
         Instantiate(wall, position4, Quaternion.identity);
 
