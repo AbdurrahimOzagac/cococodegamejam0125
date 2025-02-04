031964afec9dfcfe7b34636d243298f0eb81bbcc
Berkay ÖRENE <oreneberkay@gmail.com>
2025-02-04 13:45:45 +0300

    Room generator updated

--- CocoCode_Jam/Assets/Scripts/Room Generator.cs
+++ CocoCode_Jam/Assets/Scripts/Room Generator.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 
 public class RoomGenerator : MonoBehaviour
 {
+    
+
+    [SerializeField] private GameObject pillar;
+    [SerializeField] private GameObject wallEntrance;
+    [SerializeField] private GameObject door;
+    [SerializeField] private GameObject floor;
+    [SerializeField] private GameObject lamp;
     [SerializeField] private GameObject wall;
-    [SerializeField] private GameObject wallWithDoor;
-    [SerializeField] private GameObject ground;
-    [SerializeField] private GameObject ceil;
+    [SerializeField] private GameObject wallEntranceDoor;
 
     private int roomWidth1;
     private int roomWidth2;
+
+    private int widthWallNumber;
+    private int lenghtWallNumber;
     private int height = 10;
+    private int wallwidth = 2;
 
     // they are for test. delete after that
     [SerializeField] private GameObject player;
@@ -33,27 +42,29 @@ public class RoomGenerator : MonoBehaviour
 
     public void GenerateRoom(Vector3 playerPosition) // call it from player
     {
-        roomWidth1 = Random.Range(10, 21);
-        roomWidth2 = Random.Range(10, 21);
+        widthWallNumber = Random.Range(6, 11);
+        lenghtWallNumber = Random.Range(10, 16);
 
         Vector3 position1 = new Vector3(playerPosition.x + roomWidth1 / 2 + 1.5f, playerPosition.y, playerPosition.z + 5);
         Vector3 position2 = new Vector3(playerPosition.x - roomWidth1 /2 - 1.5f, playerPosition.y, playerPosition.z + 5);
         Vector3 position3 = new Vector3(playerPosition.x + roomWidth1 , playerPosition.y, playerPosition.z + 5 + roomWidth2/2);
         Vector3 position4 = new Vector3(playerPosition.x - roomWidth1 , playerPosition.y, playerPosition.z + 5 + roomWidth2/2);
 
-         
 
-        Instantiate(wallWithDoor, new Vector3(playerPosition.x, playerPosition.y, playerPosition.z + 5), Quaternion.identity);
-        GameObject wallWithDoor1 = GameObject.Find("WallDoor(Clone)");
-        GameObject wallWithDoor1child1 = wallWithDoor1.gameObject.transform.Find("Wall 1").gameObject;
-        GameObject wallWithDoor1child2 = wallWithDoor1.gameObject.transform.Find("Wall 3").gameObject;
-        wallWithDoor1child1.transform.position = position1;
-        wallWithDoor1child2.transform.position = position2;
+        Vector3 startPositionOfRoom = new Vector3(playerPosition.x, playerPosition.y, playerPosition.z + 5);
+        Instantiate(wallEntranceDoor, startPositionOfRoom, Quaternion.identity);
+        for(int i = -widthWallNumber; i <= widthWallNumber; i++)
+        {
+            if(i != 0)
+            {
+                Instantiate(wall, startPositionOfRoom + new Vector3(i * wallwidth, 0, 0), Quaternion.identity);
+            }
+        }
+        Instantiate(pillar, startPositionOfRoom + new Vector3(1 + widthWallNumber* wallwidth, 0, 0), Quaternion.identity);
+        Instantiate(pillar, startPositionOfRoom - new Vector3(1 + widthWallNumber * wallwidth, 0, 0), Quaternion.identity);
 
-        
 
-        Instantiate(wall, position3, Quaternion.identity);
-        Instantiate(wall, position4, Quaternion.identity);
+
 
     }
 
